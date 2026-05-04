8a316b695cffe7028a565105e0886130deeeb910
Zink0909 <majiaju89@gmail.com>
2026-05-04 16:58:28 -0700

    add startup log

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const WebSocket = require('ws');
+console.log('Starting Bags ReStream service...');
 
 const BAGS_API_KEY = process.env.BAGS_API_KEY;
 const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
